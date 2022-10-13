8e110cbcae0eea030f7a773c053c887ee11363c4
Suraj <surajaskmini@gmail.com>
2022-10-13 17:46:50 +0545

    FastAPI POST, Testing Method in Database

---
+++ Test/test_get_data.py
@@ -0,0 +1,21 @@
+from fastapi.testclient import TestClient
+from unittest.mock import Mock
+
+from database_store import app,get_data
+from schemas import InputEmpDetails
+
+client= TestClient(app)
+
+def test_get_data():
+    mock_session=Mock()
+    response=InputEmpDetails(employee_id="Emp 6", employee_name= "Employee 6", employee_position= "Senior Developer", employee_depart= "Quality Assurance")
+    result=get_data(response, session=mock_session)
+
+    mock_session.add.assert_called_once()
+    mock_session.commit.assert_called_once()
+    mock_session.refresh.assert_called_once()
+    assert isinstance(result,InputEmpDetails)
+    assert result.employee_id=="Emp 6"
+    assert result.employee_name=="Employee 6"
+    assert result.employee_position=="Senior Developer"
+    assert result.employee_depart=="Quality Assurance"
---
+++ Test/test_team_data.py
@@ -0,0 +1,21 @@
+from fastapi.testclient import TestClient
+from unittest.mock import Mock
+
+from database_store import app,get_team_data
+from schemas import InputTeamDetails
+
+client= TestClient(app)
+
+def test_get_data():
+    mock_session=Mock()
+    response=InputTeamDetails(employee_id="Emp 4", employee_name= "Employee 4", team_name="Team-Frontend", team_manager="Manager 2")
+    result=get_team_data(response, session=mock_session)
+
+    mock_session.add.assert_called_once()
+    mock_session.commit.assert_called_once()
+    mock_session.refresh.assert_called_once()
+    assert isinstance(result,InputTeamDetails)
+    assert result.employee_id=="Emp 4"
+    assert result.employee_name=="Employee 4"
+    assert result.team_name=="Team-Frontend"
+    assert result.team_manager=="Manager 2"
