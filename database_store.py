343963aa43f71861e9a57ad1ace588e7883e785f
Suraj <surajaskmini@gmail.com>
2022-10-13 17:13:16 +0545

    FastAPI POST Method in Database

--- database_store.py
+++ database_store.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlmodel import create_engine, SQLModel, Session
 
-from schemas import InputData
+from schemas import InputEmpDetails, InputTeamDetails
 
 app=FastAPI()
 
@@ -21,12 +21,23 @@ def get_session():
 def on_startup():
     SQLModel.metadata.create_all(engine)
 
-@app.post("/members/store/employee")
-def get_data(new_emp:InputData, session:Session=Depends(get_session))->InputData:
-    new_emp_data=InputData.from_orm(new_emp)
+@app.post("/members/store/employee/")
+def get_data(new_emp:InputEmpDetails, session:Session=Depends(get_session))->InputEmpDetails:
+    new_emp_data=InputEmpDetails.from_orm(new_emp)
     session.add(new_emp_data)
     session.commit()
     session.refresh(new_emp_data)
     return new_emp
 
 
+@app.post("/members/store/team/")
+def get_team_data(new_team:InputTeamDetails, session: Session=Depends(get_session))->InputTeamDetails:
+    team=session.get(InputEmpDetails,new_team.employee_id)
+    if team:
+        new_team_data=InputTeamDetails.from_orm(new_team)
+        session.add(new_team_data)
+        session.commit()
+        session.refresh(new_team_data)
+        return new_team
+    else:
+        raise HTTPException(status_code=404, detail=f"No Employee with id= {new_team.employee_id} found")
--- schemas.py
+++ schemas.py
@@ -2,11 +2,18 @@ from sqlalchemy import table
 from sqlmodel import SQLModel, Field
 
 
-class InputData(SQLModel, table=True):
+class InputEmpDetails(SQLModel, table=True):
     employee_id: str=Field(primary_key=True)
     employee_name: str
     employee_position: str
     employee_depart: str
+
+
+class InputTeamDetails(SQLModel, table=True):
+    employee_id: str= Field(primary_key=True)
+    employee_name: str
+    team_name: str
+    team_manager: str
 
 
 
